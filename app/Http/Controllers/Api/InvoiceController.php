21e9adb6393e34d575f307a1fe09c74127864233
Irham Sahbana <irhamsahbana@gmail.com>
2023-02-06 07:08:17 +0800

    main: - refactor code using switch case for list invoices - give agent access for thier congregation invoices

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -15,6 +15,7 @@ class InvoiceController extends Controller
     public function index(Request $request)
     {
         $user = $this->getUser();
+        $userCategory = $user?->person?->category?->name;
 
         $invoices = Invoice::select('*')
             ->with([
@@ -24,18 +25,22 @@ class InvoiceController extends Controller
             ])
             ->where('company_id', $user->person->company_id);
 
-        if ($user->person->category->name === 'director') {
-            if (!empty($request->branch_id)) $invoices->where('branch_id', $request->branch_id);
-            if (!empty($request->agent_id)) $invoices->where('agent_id', $request->agent_id);
-            if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
-        } else if ($user->person->category->name === 'branch-manager') {
-            $invoices->where('branch_id', $user->person->branch_id);
+        switch ($userCategory) {
+            case 'director':
+                if (!empty($request->branch_id)) $invoices->where('branch_id', $request->branch_id);
+                if (!empty($request->agent_id)) $invoices->where('agent_id', $request->agent_id);
+                if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
+                break;
+            case 'branch-manager':
+                $invoices->where('branch_id', $user->person->branch_id);
 
-            if (!empty($request->agent_id)) $invoices->where('agent_id', $request->agent_id);
-            if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
-        } else if ($user->person->category->name === 'agent') {
-            $invoices->where('agent_id', $user->person->id);
-            if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
+                if (!empty($request->agent_id)) $invoices->where('agent_id', $request->agent_id);
+                if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
+                break;
+            case 'agent':
+                $invoices->where('agent_id', $user->person->id);
+                if (!empty($request->congregation_id)) $invoices->where('congregation_id', $request->congregation_id);
+                break;
         }
 
         $invoices = $invoices->paginate((int) $request->per_page ?? 15)->toArray();
@@ -57,14 +62,22 @@ class InvoiceController extends Controller
     public function show($id)
     {
         $user = $this->getUser();
+        $userCategory = $user?->person?->category?->name;
 
         $data = Invoice::with(['invoiceDetails', 'payments'])->where('id', $id);
-        if ($user->person->category->name == 'director') {
-            $data = $data->where('company_id', $user->company_id)->first();
-        } else if ($user->person->category->name == 'branch-manager') {
-            $data = $data->where('branch_id', $user->branch_id)->first();
-        } else {
-            return (new Response)->json(null, self::NOT_AUTHORIZED_MESSAGE, 403);
+
+        switch ($userCategory) {
+            case 'director':
+                $data = $data->where('company_id', $user->person->company_id)->first();
+                break;
+            case 'branch-manager':
+                $data = $data->where('branch_id', $user->person->branch_id)->first();
+                break;
+            case 'agent':
+                $data = $data->where('agent_id', $user->person->id)->first();
+                break;
+            default:
+                return (new Response)->json(null, self::NOT_AUTHORIZED_MESSAGE, 403);
         }
 
         if (!$data) return (new Response)->json(null, 'Invoice not found', 404);
